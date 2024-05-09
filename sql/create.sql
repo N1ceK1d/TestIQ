a5f781b99b6fa4a4abf4a7c873a2d929ad340a9f
N1ceK1d <retr0raven666@gmail.com>
2024-05-09 17:57:51 +0500

    update insert sql

--- sql/create.sql
+++ sql/create.sql
@@ -2,3 +2,11 @@ CREATE TABLE Questions (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     question_text VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE Answers (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    answer_text VARCHAR(255) NOT NULL,
+    question_id INT NOT NULL,
+    is_correct BOOLEAN NOT NULL,
+    answer_char VARCHAR(1) NOT NULL,
+);
